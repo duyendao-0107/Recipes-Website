bc4edc38ab401771f8f67565c57e2a72ef1350c2
duyendao-0107 <n19dcpt009@student.ptithcm.edu.vn>
2023-08-06 07:34:19 +0700

    Updated

--- recipe_website/recipe/models.py
+++ recipe_website/recipe/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.utils.text import slugify
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.conf import settings
 
